4d033269f86f43358df4b59ef3bd54f892487adc
emirgulsacan <egulsacan@innova.com.tr>
2024-07-17 11:55:23 +0300

    Add project files.

---
+++ AuthAPI/Controllers/AuthsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AuthAPI.Business;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace AuthAPI.Controllers
+{
+   [ApiController]
+   [Route("api/[controller]")]
+   public class AuthsController : ControllerBase
+   {
+      IConfiguration _configuration;
+      public AuthsController(IConfiguration configuration)
+      {
+         _configuration = configuration;
+      }
+      public IActionResult Login(string userName, string password)
+      {
+         TokenHandler._configuration = _configuration;
+         return Ok(userName == _configuration["AuthUser:UserName"] && password == _configuration["AuthUser:Password"] ? TokenHandler.CreateAccessToken() : new UnauthorizedResult());
+      }
+   }
+}
---
+++ CostumerAPI/Controllers/CustomersController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CostumerAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class CustomersController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            return Ok(new List<string> {
+            "John", "Micheal", "Lisa", "Elizabeth", "Murray"
+         });
+        }
+    }
+}
